84023e52b0e022d5cc3ce6f3ee11980ab7564fdd
Daniel Osaku <danosaku@hotmail.com>
2018-01-25 16:28:55 -0200

    Update finetune.py

--- caffenet/finetune.py
+++ caffenet/finetune.py
@@ -18,14 +18,10 @@ if __name__ == '__main__':
 
     img_rows, img_cols = 227, 227 # Resolution of inputs
     channel = 3
-    #num_classes = 9
-    #batch_size = 16 
-    #nb_epoch = 10
     batch_size, nb_epoch, num_classes, save_weights = config.Load()
     # Load Cifar10 data. Please implement your own load_data() module for your own dataset
     X_train, Y_train, X_valid, Y_valid = load_cifar10_data(img_rows, img_cols)
-    #X_train, Y_train, X_valid, Y_valid = Generate_train_test2.load_dataset_theano(3, "eggs_train.txt", "eggs_test.txt", num_classes, img_rows)
-    #X_train, Y_train, X_valid, Y_valid = config.Load_dataset(img_rows, num_classes, channel, "Theano")
+    
     model = CaffeNet(weights='caffenet_weights_th.h5', classes=num_classes)
 
     # Start Fine-tuning
