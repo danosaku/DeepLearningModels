40e0dcadddddfd1116e2cad726ffc63a90a380d2
Daniel Osaku <danosaku@hotmail.com>
2018-07-06 11:16:18 -0300

    Update config.py

--- caffenet/config.py
+++ caffenet/config.py
@@ -22,3 +22,7 @@ def Get_filename_weights_to_save(model, mb, ne):
   else:
     filename = model+"_mb_"+str(mb)+"_ne_"+str(ne)+"_without_pretrain_weights("+str(data.day)+"-"+str(data.month)+"-"+str(data.year)+"-"+str(data.hour)+"-"+str(data.minute)+").h5"  
   return filename
+
+
+def load_target_names():
+    return ['Class 1','Class 2','Class 3','Class 4','Class 5','Class 6','Class 7','Class 8','Class 9','Class 10']
